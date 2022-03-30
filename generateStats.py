95f712f2e15e8f33d71c45179bb299efdd3ce179
bigfoot1144 <cole.sutyak@gmail.com>
2022-03-30 01:34:51 -0400

    update generate stats to find stats of each output class instead of as a whole

--- generateStats.py
+++ generateStats.py
@@ -8,29 +8,39 @@ print("Features and attributes of dataset: ", riceAMLObj.shape)
 featureList = ["AREA", "PERIMETER", "MAJOR_AXIS", "MINOR_AXIS", "ECCENTRICITY", "EQDIASQ","SOLIDITY","CONVEX_AREA", "EXTENT","ASPECT_RATIO","ROUNDNESS","COMPACTNESS","SHAPEFACTOR_1","SHAPEFACTOR_2","SHAPEFACTOR_3","SHAPEFACTOR_4"]
 #creating rice stats csv
 f = open("riceStats.csv", "a")
-#writing to file
-f.write("name,max,min,mean,median,mode,standard deviation\n")
-for feature in featureList:
-    f.write(feature)
-    f.write(",")
-    f.write(str(riceAMLObj[feature].max()))
-    f.write(",")
-    f.write(str(riceAMLObj[feature].min()))
-    f.write(",")
-    f.write(str(riceAMLObj[feature].mean()))
-    f.write(",")
-    f.write(str(riceAMLObj[feature].median()))
-    f.write(",")
-    for mode in riceAMLObj[feature].mode():
-        f.write(str(mode))
-        f.write(" ")
-    f.write(",")
-    f.write(str(riceAMLObj[feature].std()))
-    f.write("\n")
 
-    print(feature, " Max:", riceAMLObj[feature].max())
-    print(feature, " Min:", riceAMLObj[feature].min())
-    print(feature, " Mean:", riceAMLObj[feature].mean())
-    print(feature, " Median:", riceAMLObj[feature].median())
-    print(feature, " Mode:", riceAMLObj[feature].mode())
-    print(feature, " Std:", riceAMLObj[feature].std())
+classList = ["Basmati", "Arborio", "Jasmine", "Ipsala", "Karacadag"]
+
+for item in classList:
+
+    riceAMLObjSubset = riceAMLObj.loc[(riceAMLObj["CLASS"] == item)]
+    print(item)
+    print(riceAMLObjSubset)
+    f.write(item)
+    f.write('\n')
+    #writing to file
+    f.write("name,max,min,mean,median,mode,standard deviation\n")
+    for feature in featureList:
+        f.write(feature)
+        f.write(",")
+        f.write(str(riceAMLObjSubset[feature].max()))
+        f.write(",")
+        f.write(str(riceAMLObjSubset[feature].min()))
+        f.write(",")
+        f.write(str(riceAMLObjSubset[feature].mean()))
+        f.write(",")
+        f.write(str(riceAMLObjSubset[feature].median()))
+        f.write(",")
+        for mode in riceAMLObjSubset[feature].mode():
+            f.write(str(mode))
+            f.write(" ")
+        f.write(",")
+        f.write(str(riceAMLObjSubset[feature].std()))
+        f.write("\n")
+
+        #print(feature, " Max:", riceAMLObjSubset[feature].max())
+        #print(feature, " Min:", riceAMLObjSubset[feature].min())
+        #print(feature, " Mean:", riceAMLObjSubset[feature].mean())
+        #print(feature, " Median:", riceAMLObjSubset[feature].median())
+        #print(feature, " Mode:", riceAMLObjSubset[feature].mode())
+        #print(feature, " Std:", riceAMLObjSubset[feature].std())
